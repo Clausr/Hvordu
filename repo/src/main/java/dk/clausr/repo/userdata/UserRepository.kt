a9e98ef9325a8cf1bea9c8e195a831ecffb80e18
Claus Rasmussen <decoid@gmail.com>
2024-05-03 08:55:30 +0200

    UserData can be null..

--- repo/src/main/java/dk/clausr/repo/userdata/UserRepository.kt
+++ repo/src/main/java/dk/clausr/repo/userdata/UserRepository.kt
@@ -3,6 +3,7 @@ package dk.clausr.repo.userdata
 import dk.clausr.core.models.UserData
 import dk.clausr.koncert.data.UserPreferencesDataSource
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -10,7 +11,13 @@ import javax.inject.Singleton
 class UserRepository @Inject constructor(
     private val userSettingDataSource: UserPreferencesDataSource,
 ) {
-    fun getUserData(): Flow<UserData?> = userSettingDataSource.userData
+    fun getUserData(): Flow<UserData?> = userSettingDataSource.userData.map {
+        if (it.username.isBlank() && it.group.isBlank()) {
+            null
+        } else {
+            it
+        }
+    }
 
     suspend fun setUserData(userData: UserData) {
         userSettingDataSource.setUserPreferences(userData)
